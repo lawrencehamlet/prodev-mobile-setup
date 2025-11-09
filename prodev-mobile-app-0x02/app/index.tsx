345b8faa8ea0047138ef3c8b9fc9f83acce40646
lawrencehamlet <reallawrencehamlet@gmail.com>
2025-11-09 05:03:30 +0000

    fix: replace missing background-image.png with sample-image.png in all projects

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <ImageBackground
-          source={require('@/assets/images/background-image.png')}
+          source={require('@/assets/images/sample-image.png')}
           style={styles.background}
           resizeMode="cover"
         >
